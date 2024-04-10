7ed4e9e12617e5d3dfbceb5c09cc7e536ba4954a
Be The One/ <156409357+biwanshrestha14@users.noreply.github.com>
2024-04-10 12:46:56 +0000

    function learned

---
+++ functions.js
@@ -0,0 +1,19 @@
+// const myarray =[100,200,300,400]
+// function biwan(pass){
+//     console.log(`the value you have passes is ${pass[2]}`)
+// }
+// biwan(myarray)
+
+
+
+// const fantech = {
+// brand:"fantech",
+// product:"mouse",
+// price:1699
+// }
+function tech(goliath){
+    console.log(`The name of the product is ${goliath.brand} Thor II X16 and the price is ${goliath.price}`);
+}
+tech(fantech={brand:"fantech",
+ product:"mouse",
+ price:1699})
